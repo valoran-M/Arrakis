51b12720a835c4843e562db466a9b66f3c4ad134
valomay <valomay@protonmail.com>
2023-08-19 11:42:30 +0200

    add ;

--- test/sim_test/tests.ml
+++ test/sim_test/tests.ml
@@ -4,5 +4,5 @@ let tests : unit Alcotest.test list =
   [ "Simulator", [
       test_case "R_instruction" `Quick R_instruction.test;
       test_case "I_instruction_arith" `Quick I_instruction.test_arith;
-      test_case "I_instruction_load"  `Quick I_instruction.test_load
+      test_case "I_instruction_load"  `Quick I_instruction.test_load;
   ] ]

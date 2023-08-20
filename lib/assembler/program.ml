faeeb13b09b849bef718a9f2cd739c7fa2ac38b8
valomay <valomay@protonmail.com>
2023-08-20 15:12:20 +0200

    added program

---
+++ lib/assembler/program.ml
@@ -0,0 +1,63 @@
+(* ---------------------------- All instructions ---------------------------- *)
+
+type r_instruction =
+  | ADD | SUB
+  | XOR | OR  | AND
+  | SLL | SRL | SRA
+  | SLT | SLTU
+
+type i_instruction =
+  (* arit *)
+  | ADDI
+  | XORI | ORI  | ANDI
+  | SLLI | SRLI | SARI
+  | SLTI | SLTIU
+  (* load *)
+  | LB  | LH  | LW
+  | LBU | LHU
+  (* other *)
+  | JALR
+  | ECALL
+
+type s_instruction =
+  (* store *)
+  | SB | SH | SW
+
+type b_instruction =
+  (* branch *)
+  | BEQ  | BNE
+  | BLT  | BGE
+  | BLTU | BGEU
+
+type u_instruction =
+  | LUI | AUIPC
+
+type j_instruction =
+  | JAL
+
+(* ------------------------------- Programme -------------------------------- *)
+
+type imm =
+  | Label of string 
+  | Imm   of int32
+
+type instruction =
+                      (* rd    rs1   rs2 *)
+  | R of r_instruction * int * int * int
+                      (* rd    rs1   imm *)
+  | I of i_instruction * int * int * imm
+                      (* rs2   rs1   imm *)
+  | S of s_instruction * int * int * imm
+                      (* rs1   rs2   imm *)
+  | B of b_instruction * int * int * imm
+                      (* rd    imm *)
+  | U of u_instruction * int * imm
+                      (* rd    imm *)
+  | J of j_instruction * int * imm
+
+type program =
+  | Seq   of program * program
+  | Inst  of instruction
+  | Label of string
+
+

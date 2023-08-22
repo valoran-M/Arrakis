8d40c3e4f8833066353a410276de1552fff4fdb4
valomay <valomay@protonmail.com>
2023-08-22 09:03:46 +0200

    rewrite tests

--- test/sim_test/tests.ml
+++ test/sim_test/tests.ml
@@ -1,7 +1,7 @@
 open Alcotest
 
-let tests : unit Alcotest.test list =
-  [ "Simulator", [
+let tests : unit Alcotest.test =
+   "Simulator", [
       test_case "R_instruction" `Quick R_instruction.test;
       test_case "R_instruction_mul" `Quick R_instruction.test_mul;
       test_case "I_instruction_arith" `Quick I_instruction.test_arith;
@@ -11,4 +11,4 @@ let tests : unit Alcotest.test list =
       test_case "B_instruction"  `Quick B_instruction.test;
       test_case "U_instruction"  `Quick U_instruction.test;
       test_case "J_instruction"  `Quick J_instruction.test;
-  ] ]
+  ]

92389a45159ea3cd46f8dbeb21ae622e2c44cbad
valomay <valomay@protonmail.com>
2023-08-22 12:42:02 +0200

    added error file and test for lexer

--- bin/main.ml
+++ bin/main.ml
@@ -1,3 +1,13 @@
 let cpu = Simulator.Cpu.make 0l
 
 let memory = Simulator.Memory.make ()
+
+let code = {|addi x1 x0 10
+|}
+
+let code =
+  Assembler.Lexer.prog 0 (Lexing.from_string code)
+
+let () = match code with
+  | Seq (Instr (l, I (ADDI, _, _, _)), Nil) -> Printf.printf "%d\n" l
+  | _ -> assert false

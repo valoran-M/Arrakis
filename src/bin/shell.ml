bd8a91ce34e97faf722e3054d2342894c20a4f88
valomay <valomay@protonmail.com>
2023-08-23 18:25:51 +0200

    up shell

--- src/bin/shell.ml
+++ src/bin/shell.ml
@@ -1,1 +1,7 @@
-let run _arch = ()
+let rec run arch =
+  Printf.printf "> ";
+  let line = read_line () in
+  let words = String.split_on_char ' ' line in
+  match words with
+  | command :: _args -> Printf.printf "%s\n" command
+  | _ -> run arch

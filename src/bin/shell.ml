b4bc095c7465faf97279b34007eb41d144a5843e
valomay <valomay@protonmail.com>
2023-08-24 22:32:51 +0200

    added base command

--- src/bin/shell.ml
+++ src/bin/shell.ml
@@ -6,16 +6,32 @@ let rec run arch =
   | Zero -> Printf.printf "Waring: not syscal end\n"
   | Sys_call -> failwith "TODO"
 
+let print_help () =
+  print_string {|
+Commandes :
+
+(r)un -> run code
+
+(b)reakpoint -> create breakpoint
+(s)tep -> In debug mode execute next instruction
+(n)ext -> run to next breakpoint
+(sh)ow -> show args
+
+(e)xit
+|}
+
 let parse_command arch command _args =
   match command with
-  | "run" -> run arch
-  | _ -> Printf.printf "%s\n" command
+  | "run" | "r"  -> run arch
+  | "help" -> print_help ()
+  | _ -> Printf.printf "Undefined command: \"%s\".  Try \"help\".\n" command
 
 let rec shell arch =
   Printf.printf "> ";
   let line = read_line () in
   let words = String.split_on_char ' ' line in
   match words with
-  | command :: args -> parse_command arch command args
+  | "exit" :: _ | "e" :: _ -> ()
+  | command :: args -> parse_command arch command args; shell arch
   | _ -> shell arch
 

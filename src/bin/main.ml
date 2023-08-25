caba673f9188e5eed18cd214e20dd0ea208d2922
Gurvan <gurvan@debauss.art>
2023-08-25 17:59:35 +0200

    fix valorant

--- src/bin/main.ml
+++ src/bin/main.ml
@@ -1,15 +1,8 @@
-let usage_msg = "arrakis <file>"
-
-let input_file = ref ""
-let anon_fun filename = input_file := filename
-
-let speclist = []
-
+open Options
 open Simulator
 
 let () =
-  Arg.parse speclist anon_fun usage_msg;
-  let channel = open_in !input_file in
+  let channel = open_in input_file in
   let mem, _, label, debug =
     Assembler.Translate.translate (Lexing.from_channel channel)
   in

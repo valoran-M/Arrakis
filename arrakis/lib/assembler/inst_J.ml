407661ecc89046be7ebe156a07b3bf64515c77a4
valomay <valomay@protonmail.com>
2023-09-12 16:28:34 +0200

    fix j bug

--- arrakis/lib/assembler/inst_J.ml
+++ arrakis/lib/assembler/inst_J.ml
@@ -19,10 +19,10 @@ let write_in_memory mem addr instruction rd imm =
   let (<<) = Int32.shift_left in
   let (||) = Int32.logor in
   let (opcode, _) = Hashtbl.find j_instructions instruction in
-  let imm20    = Utils.get_interval imm 12 12 in
-  let imm19_12 = Utils.get_interval imm 10 5  in
+  let imm20    = Utils.get_interval imm 20 20 in
+  let imm19_12 = Utils.get_interval imm 19 12 in
   let imm11    = Utils.get_interval imm 11 11 in
-  let imm10_1  = Utils.get_interval imm 4  1  in
+  let imm10_1  = Utils.get_interval imm 10 1  in
   let code = (imm20 << 31)    || (imm10_1 << 21) || (imm11 << 20) ||
              (imm19_12 << 12) || (rd << 7) || opcode in
   Simulator.Memory.set_int32 mem addr code

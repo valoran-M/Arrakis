1b7b5458d291ff1f51d3914028ae7e05a9a7bb1f
valomay <valomay@protonmail.com>
2023-09-15 17:07:57 +0200

    up calculator

--- examples/polish_calc.s
+++ examples/polish_calc.s
@@ -6,14 +6,56 @@
   line_size: .word 256
   line:      .zero 256
 
+  print:     .zero 5
+             .asciiz "\n"
+
   plus: .asciiz "plus\n"
 
 # /!\ Norms for this assembly code
 #
 # s1 : calculator's stack pointer
 # s2 : calculator's stack size
+# s3 : line's address
 
 .text
+
+# Utils ########################################################################
+
+# Print register
+# a0 : register to print
+print_reg:
+      la a1, print
+      addi a1, a1, 19
+      li a2, 2
+      li t1, 10
+      .while_print_reg:
+          rem t2, a0, t1
+          addi t2, t2, 48
+          sb t2, 0(a1)
+          div a0, a0, t1
+          addi a1, a1, -1
+          addi a2, a2, 1
+          bnez a0, .while_print_reg
+      li a7, 64
+      li a0, 1
+      ecall
+      ret
+
+# test if a character is a digit
+# a0 : character
+#
+# return :
+# a0 : digit if is a digit else -1
+is_digit:
+      li t0, 48
+      blt a0, t0, .false_digit
+      li t0, 58
+      bge a0, t0, .false_digit
+      addi a0, a0, -48
+      ret
+    .false_digit:
+      li a0, -1
+      ret
 
 # Stack ########################################################################
 
@@ -55,6 +97,20 @@ pop_stack:
       li a0, 0
       ret
 
+# Get first element of the stack
+#
+# return:
+# a0 : 1 if succeed else 0
+# a1 : first argument
+first_stack:
+      blez s2, .stack_empty
+      lw a1 -4(s1)
+      li a0, 1
+      ret
+  .stack_empty:
+      li a0, 0
+      ret
+
 # Operator #####################################################################
 
 exec_add:
@@ -62,11 +118,43 @@ exec_add:
       sw ra, 0(sp)
       addi sp, sp, -4
 
-      li a7, 64
-      li a0, 1
-      la a1, plus
-      li a2, 6
-      ecall
+      call pop_stack
+      mv t0, a1
+      call pop_stack
+      add a0, t0, a1
+      call push_stack
+
+      # pop ra
+      lw ra, 4(sp)
+      addi sp, sp, 4
+      ret
+
+exec_sub:
+      # push ra
+      sw ra, 0(sp)
+      addi sp, sp, -4
+
+      call pop_stack
+      mv t0, a1
+      call pop_stack
+      sub a0, t0, a1
+      call push_stack
+
+      # pop ra
+      lw ra, 4(sp)
+      addi sp, sp, 4
+      ret
+
+exec_mult:
+      # push ra
+      sw ra, 0(sp)
+      addi sp, sp, -4
+
+      call pop_stack
+      mv t0, a1
+      call pop_stack
+      mul a0, t0, a1
+      call push_stack
 
       # pop ra
       lw ra, 4(sp)
@@ -105,32 +193,96 @@ get_line:
       ecall
       ret
 
+# Read line for get an integer read on (s3) string
+# a0 : first digit
+#
+# return:
+# a0 : digit
+get_digit:
+      # push ra
+      sw ra, 0(sp)
+      addi sp, sp, -4
+      mv a1, a0
+      .while_get_digit:
+          lb a0, 0(s3)
+          call is_digit
+          bltz a0, .end_get_digit
+          li t0, 10
+          mul a1, a1, t0
+          add a1, a1, a0
+          addi s3, s3, 1
+          j .while_get_digit
+      .end_get_digit:
+      # pop ra
+      lw ra, 4(sp)
+      addi sp, sp, 4
+      mv a0, a1
+      ret
+
 exec_op:
       # push ra
       sw ra, 0(sp)
       addi sp, sp, -4
-      la s2, line
+      la s3, line
 
     .while_exec:
-      mv a0, s2
+      mv a0, s3
       call skip_space
-      lb t1, 0(s2)
+      mv s3, a0
+      lb t1, 0(s3)
 
       beqz t1, .zero_exec
 
+      mv a0, t1
+      call is_digit
+      bgez a0, .digit_op
+
       xori t0, t1, 43     # if t1 = '+'
       beqz t0, .add_op
+
+      xori t0, t1, 45     # if t1 = '-'
+      beqz t0, .sub_op
+
+      xori t0, t1, 42     # if t1 = '*'
+      beqz t0, .mul_op
+
+      xori t0, t1, 113    # if t1 = 'q'
+      beqz t0, .end
+
+      xori t0, t1, 112    # if t1 = 'q'
+      beqz t0, .print_op
+
+      j .continue_exec
+
+    .digit_op:
+      addi s3, s3, 1
+      call get_digit
+      call push_stack
+      j .continue_exec
+
+    .print_op:
+      call first_stack
+      mv a0, a1
+      call print_reg
+      j .continue_exec
+
+    .sub_op:
+      call exec_sub
+      j .continue_exec
+
+    .mul_op:
+      call exec_mult
       j .continue_exec
 
     .add_op:
       call exec_add
 
     .continue_exec:
-      addi s2, s2, 1
+      addi s3, s3, 1
       j .while_exec
     .zero_exec:
       call get_line
-      la s2, line
+      la s3, line
       j .while_exec
     .end:
       # pop ra

35a118905e924b8d3892c776f2f67b7926f141e9
Gurvan <gurvan@debauss.art>
2023-11-30 09:34:31 +0100

    Removed '.' at end of documentation for option

--- arrakis/bin/options.ml
+++ arrakis/bin/options.ml
@@ -17,13 +17,13 @@ let env = ref "unix"
 let spec = [
   ("-U",            Arg.Set unix_socket,       " Use unix socket"                  );
   ("-f",            Arg.Set_string unix_file , " Unix socket's file"               );
-  ("-e",            Arg.Set_string env,        "<venus|unix> Set env for ecalls."  );
+  ("-e",            Arg.Set_string env,        "<venus|unix> Set env for ecalls"   );
   ("--no-color",    Arg.Set no_color,          " Don't use color in output"        );
-  ("--allow-root",  Arg.Set allow_root,        " Allow usage in root mode."        );
-  ("-ns",           Arg.Set no_shell,          " Run the program and exit."        );
-  ("--no-shell",    Arg.Set no_shell,          " Run the program and exit."        );
-  ("-v",            Arg.Set show_version,      " Show version number and exit."    );
-  ("--version",     Arg.Set show_version,      " Show version number and exit."    );
+  ("--allow-root",  Arg.Set allow_root,        " Allow usage in root mode"        );
+  ("-ns",           Arg.Set no_shell,          " Run the program and exit"        );
+  ("--no-shell",    Arg.Set no_shell,          " Run the program and exit"        );
+  ("-v",            Arg.Set show_version,      " Show version number and exit"    );
+  ("--version",     Arg.Set show_version,      " Show version number and exit"    );
 ]
 
 let spec = Arg.align spec

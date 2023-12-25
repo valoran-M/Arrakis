5baed6f3353d2a6243a22b94622b8da1d43f7d1e
valomay <mvalo@proton.me>
2023-12-25 17:18:02 +0100

    change command to another branch

--- arrakis/lib/command/make.ml
+++
@@ -1,9 +0,0 @@
-module type Command = sig
-  type ret
-
-  val description : string
-  val help        : unit -> string
-
-  val exec : string list -> ret
-end
-
--- arrakis/lib/command/state.ml
+++
@@ -1,9 +0,0 @@
-type state =
-  { 
-    channel : Format.formatter;
-    history : Simulator.History.t;
-    line_debug : (int, int32)    Hashtbl.t;
-    add_debug  : (int, int32)    Hashtbl.t;
-    label      : (string, int32) Hashtbl.t;
-  }
-

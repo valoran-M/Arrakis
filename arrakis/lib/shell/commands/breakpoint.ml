1dbdf8ad55f607c5fd1cae4e05c290769249d52e
Gurvan <jurwane@protonmail.ch>
2024-03-11 19:23:54 +0100

    Use sub command system for breakpoints

--- arrakis/lib/shell/commands/breakpoint.ml
+++ arrakis/lib/shell/commands/breakpoint.ml
@@ -10,7 +10,16 @@ open Global_utils.Print
 
 exception End_loop
 
-let line_breakpoint (state : Types.state) arg =
+let iter f args (state : Types.state) =
+  (if List.length args == 0 then
+      fprintf state.out_channel "%a Command require at least one argument@." error ()
+  else
+    List.iter (f state) args);
+  state
+
+(* Line --------------------------------------------------------------------- *)
+
+let execute_line (state : Types.state) arg =
   match int_of_string_opt arg with
   | None      ->
       fprintf state.out_channel "%a '%s' is not a number.@." error () arg
@@ -26,7 +35,19 @@ let line_breakpoint (state : Types.state) arg =
       fprintf state.out_channel
         "%a Line %d does not contain code.@." error () line
 
-let addr_breakpoint (state : Types.state) arg =
+let breakpoint_line : Types.cmd = {
+  long_form   = "line";
+  short_form  = "l";
+  name        = "(l)ine";
+  short_desc  = "Add breakpoints on specified lines";
+  long_desc   = "";
+  execute     = iter execute_line;
+  sub         = [];
+}
+
+(* Addr --------------------------------------------------------------------- *)
+
+let execute_addr (state : Types.state) arg =
   let number = Hashtbl.length state.breakpoints in
   match Int32.of_string_opt arg with
   | Some addr ->
@@ -45,7 +66,37 @@ let addr_breakpoint (state : Types.state) arg =
         "%a Created breakpoint %d at 0x%x.@."
         info () number (Int32.to_int addr)
 
-let remove_breakpoint (state : Types.state) arg =
+let breakpoint_addr : Types.cmd = {
+  long_form   = "addr";
+  short_form  = "a";
+  name        = "(a)ddr";
+  short_desc  = "Add breakpoints on specified adress";
+  long_desc   = "";
+  execute     = iter execute_addr;
+  sub         = [];
+}
+
+(* Print -------------------------------------------------------------------- *)
+
+let execute_print _args (state : Types.state) =
+  Hashtbl.iter (fun addr number ->
+    let open Global_utils.Integer in
+    fprintf state.out_channel "%3d -> 0x%08x@." number (int32_to_int addr))
+  state.breakpoints
+
+let breakpoint_print : Types.cmd = {
+  long_form   = "print";
+  short_form  = "p";
+  name        = "(p)rint";
+  short_desc  = "Print all breakpoints";
+  long_desc   = "";
+  execute     = iter execute_addr;
+  sub         = [];
+}
+
+(* Remove ------------------------------------------------------------------- *)
+
+let execute_remove (state : Types.state) arg =
   try
     let breakpoint = int_of_string arg in
     Hashtbl.iter (fun addr line ->
@@ -61,34 +112,27 @@ let remove_breakpoint (state : Types.state) arg =
       fprintf state.out_channel "%a Breakpoint '%s' does not exist.@."
         error () arg
 
-let iter f l (state : Types.state) =
-  if List.length l == 0 then
-      fprintf state.out_channel "%a Command require at least one argument@." error ()
-  else List.iter f l
+let breakpoint_remove : Types.cmd = {
+  long_form   = "remove";
+  short_form  = "r";
+  name        = "(r)emove";
+  short_desc  = "Remove specified breakpoints";
+  long_desc   = "";
+  execute     = iter execute_remove;
+  sub         = [];
+}
 
-let execute args (state : Types.state) =
-  begin match args with
-  | "line"   :: args
-  | "l"      :: args -> iter (line_breakpoint state) args state
-  | "addr"   :: args
-  | "a"      :: args -> iter (addr_breakpoint state) args state
-  | "remove" :: args
-  | "r"      :: args -> iter (remove_breakpoint state) args state
-  | "print"  :: _
-  | "p"      :: _    ->
-      Hashtbl.iter (fun addr number ->
-        fprintf state.out_channel "%3d -> 0x%08x@."
-        number (Global_utils.Integer.int32_to_int addr)) state.breakpoints
-  | _ -> Help.breakpoint state.out_channel
-  end;
-  state (* As we changed it's hashtbl, the state is actually changed here *)
-
-let breakpoint : Types.cmd = {
+let rec breakpoint : Types.cmd = {
   long_form   = "breakpoint";
   short_form  = "b";
   name        = "(b)reakpoint";
   short_desc  = "Create breakpoints";
   long_desc   = "";
-  execute     = execute;
-  sub         = [];
+  execute     = (fun _ state -> Help.command breakpoint state);
+  sub         = [
+    breakpoint_addr;
+    breakpoint_line;
+    breakpoint_print;
+    breakpoint_remove;
+  ];
 }
--- arrakis/lib/shell/commands/help.ml
+++ arrakis/lib/shell/commands/help.ml
@@ -22,52 +22,6 @@ let general (state : Types.state) =
 let command (cmd : Types.cmd) (state : Types.state) =
   print (String.capitalize_ascii cmd.long_form) cmd.long_desc cmd.sub state
 
-let breakpoint channel =
-  Format.fprintf channel
-{|  @{<fg_green> Breakpoint help:@}
-
-  @{<fg_green>*@} (b)reakpoint (l)ine <l_1> ... <l_n>
-
-    Set breakpoints on specified lines.
-
-  @{<fg_green>*@} (b)reakpoint (a)ddr <b_1> ... <b_n>
-
-    Set breakpoints on specified addresses.
-
-  @{<fg_green>*@} (b)reakpoint (r)emove <b_1> ... <b_n>
-
-    Remove specified breakpoints.
-
-  @{<fg_green>*@} (b)reakpoint (p)rint
-
-    Print all breakpoints.@.|}
-
-let print channel =
-  Format.fprintf channel
-{|  @{<fg_green>Print help:@}
-
-  @{<fg_green>*@} (p)rint (m)emory <start> <nb>
-
-      Print memory segment.
-      Starts at address <start> and displays <nb> 32 bits.
-
-      <start> can either be an adress or a register.
-
-      @{<fg_yellow>default args:@}
-        <start> Starting data segement
-        <nb>    0x10
-
-  @{<fg_green>*@} (p)rint (r)egs <r_1> ... <r_n>
-
-      Display value in specified registers.
-      If no register are specified, display all of them.
-
-  @{<fg_green>*@} (p)rint (c)ode <offset> <noffset>
-
-      If no offset is specified, print code from start to finish.
-      If offset is specified, print code from pc to pc+offset.
-      If noffset is also specified, print code from pc-noffset to pc+offset.@.|}
-
 let execute args (state : Types.state) =
   match args with
   | []       -> general state

153276bddd433fe8a21402eb0d051403a78ce925
valorant <mvalo@proton.me>
2024-04-01 22:16:18 +0200

    add pa spec

--- arrakis/lib/simulator/arguments.ml
+++ arrakis/lib/simulator/arguments.ml
@@ -1,3 +1,25 @@
+(******************************************************************************)
+(* copyright 2023-2024 - arrakis contributors                                 *)
+(*                                                                            *)
+(* this file is part of arrakis, a risc-v simulator.                          *)
+(* it is distributed under the cecill 2.1 license <http://www.cecill.info>    *)
+(******************************************************************************)
 
-let write_arguments (_arch : Arch.Riscv.t) _args =
+open Arch
+
+(*
+  Argument Spec :
+  
+    sp    +-addresses-+     +-------------arguments-------------+
+    |     |           |     |                                   |
+  +----+-----+-----+-----+-----+-----+-----+-----+-----+-----+-----+
+<-| nb | *a1 | ... | *an |a1[0]| ... |a1[x]| ... |an[0]| ... |an[z]| stack_begin
+  +----+-----+-----+-----+-----+-----+-----+-----+-----+-----+-----+
+                                                                |
+                                                           0x7fff_fff0
+ *)
+
+let write_arguments (arch : Riscv.t) (_args : string list) =
+  let _sp = Cpu.get_reg arch.cpu 0x2 in
+  (* TODO *)
   assert false
--- arrakis/lib/simulator/arguments.mli
+++ arrakis/lib/simulator/arguments.mli
@@ -1,3 +1,10 @@
+(******************************************************************************)
+(* copyright 2023-2024 - arrakis contributors                                 *)
+(*                                                                            *)
+(* this file is part of arrakis, a risc-v simulator.                          *)
+(* it is distributed under the cecill 2.1 license <http://www.cecill.info>    *)
+(******************************************************************************)
+
 
 val write_arguments : Arch.Riscv.t -> string list -> unit
 (** [set_arguments arch args] write programme set_arguments in memory

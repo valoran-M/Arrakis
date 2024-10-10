28febd089dc258cff7d10a7e9e7fe570fe022fcf
Gurvan Debaussart <gurvan@debauss.art>
2024-10-10 11:02:20 +0200

    fix: update colorsh dependency

--- arrakis/lib/colorsh/colorsh.ml
+++ arrakis/lib/colorsh/colorsh.ml
@@ -1,100 +1,87 @@
-(** colorsh - *****************************************************************)
-(*  Copyright 2023-2024 Gurvan Debaussart (https://debauss.art)               *)
-(*  This file is distributed under the MIT license                            *)
+(* colorsh ********************************************************************)
+(* Copyright 2023-2024 Gurvan Debaussart (https://debauss.art)                *)
+(* Distributed under the MIT license                                          *)
 (******************************************************************************)
 
 open Format
 
-let tag_table = Hashtbl.create 22
-let inited    = ref false
-
-let init () =
-  inited := true;
-
-  (* Foreground Color ------------------------------------------------------- *)
-
-  List.iter (fun (tag, sint) ->
-    let ostr = sprintf  "\x1b[%dm" sint in
-    let cstr =          "\x1b[39m"      in
-    Hashtbl.add tag_table tag (ostr, cstr)
-  )
-  [
-    (* Tag        Open *)
-    "fg_black",   30;
-    "fg_red",     31;
-    "fg_green",   32;
-    "fg_yellow",  33;
-    "fg_blue",    34;
-    "fg_purple",  35;
-    "fg_cyan",    36;
-    "fg_white",   37;
-  ];
-
-  (* Background Color ------------------------------------------------------- *)
-
-  List.iter (fun (tag, sint) ->
-    let ostr = sprintf  "\x1b[%dm" sint in
-    let cstr =          "\x1b[49m"      in
-    Hashtbl.add tag_table tag (ostr, cstr)
-  )
-  [
-    (* Tag        Open *)
-    "bg_black",   40;
-    "bg_red",     41;
-    "bg_green",   42;
-    "bg_yellow",  43;
-    "bg_blue",    44;
-    "bg_purple",  45;
-    "bg_cyan",    46;
-    "bg_white",   47;
-  ];
-
-  (* Text style ------------------------------------------------------------- *)
-
-  List.iter (fun (tag, oint, cint) ->
-    let ostr = sprintf "\x1b[%dm" oint in
-    let cstr = sprintf "\x1b[%dm" cint in
-    Hashtbl.add tag_table tag (ostr, cstr)
-  )
-  [
-    (* Tag       Open   Close  *)
-    "bold",      1,     22;
-    "dim",       2,     22;
-    "italic",    3,     23;
-    "underline", 4,     24;
-    "reversed",  7,     27;
-    "strike",    9,     29;
-  ]
-
-(* -------------------------------------------------------------------------- *)
-
 let open_tag = function
-  | Format.String_tag s ->
-      begin match Hashtbl.find_opt tag_table s with
-      | Some (otag, _) -> otag
-      | None           -> ""
-      end
-  | _ -> ""
+    | String_tag s ->
+        begin match s with
+        (* Foreground colors *)
+        | "fg_black"    -> "\x1b[30m"
+        | "fg_red"      -> "\x1b[31m"
+        | "fg_green"    -> "\x1b[32m"
+        | "fg_yellow"   -> "\x1b[33m"
+        | "fg_blue"     -> "\x1b[34m"
+        | "fg_purple"   -> "\x1b[35m"
+        | "fg_cyan"     -> "\x1b[36m"
+        | "fg_white"    -> "\x1b[37m"
+        (* Background color *)
+        | "bg_black"    -> "\x1b[40m"
+        | "bg_red"      -> "\x1b[41m"
+        | "bg_green"    -> "\x1b[42m"
+        | "bg_yellow"   -> "\x1b[43m"
+        | "bg_blue"     -> "\x1b[44m"
+        | "bg_purple"   -> "\x1b[45m"
+        | "bg_cyan"     -> "\x1b[46m"
+        | "bg_white"    -> "\x1b[47m"
+        (* Text style *)
+        | "bold"        -> "\x1b[1m"
+        | "dim"         -> "\x1b[2m"
+        | "italic"      -> "\x1b[3m"
+        | "underline"   -> "\x1b[4m"
+        | "reversed"    -> "\x1b[7m"
+        | "strike"      -> "\x1b[9m"
+        (* Unrecognized *)
+        | _ -> ""
+        end
+    | _ -> ""
 
 let close_tag = function
-  | Format.String_tag s ->
-      begin match Hashtbl.find_opt tag_table s with
-      | Some (_, ctag) -> ctag
-      | None           -> ""
-      end
-  | _ -> ""
+    | String_tag s ->
+        begin match s with
+        (* Foreground colors *)
+        | "fg_black"
+        | "fg_red"
+        | "fg_green"
+        | "fg_yellow"
+        | "fg_blue"
+        | "fg_purple"
+        | "fg_cyan"
+        | "fg_white"    -> "\x1b[39m"
+        (* Background color *)
+        | "bg_black"
+        | "bg_red"
+        | "bg_green"
+        | "bg_yellow"
+        | "bg_blue"
+        | "bg_purple"
+        | "bg_cyan"
+        | "bg_white"    -> "\x1b[49m"
+        (* Text style *)
+        | "bold"        -> "\x1b[22m"
+        | "dim"         -> "\x1b[22m"
+        | "italic"      -> "\x1b[23m"
+        | "underline"   -> "\x1b[24m"
+        | "reversed"    -> "\x1b[27m"
+        | "strike"      -> "\x1b[29m"
+        (* Unrecognized *)
+        | _ -> ""
+        end
+    | _ -> ""
 
 let setup_fmt fmt =
-  (if not !inited then init ());
-  pp_set_tags fmt true;
-  let sf = pp_get_formatter_stag_functions fmt () in
-  pp_set_formatter_stag_functions fmt
-  {
-    sf with
-    mark_open_stag  = open_tag;
-    mark_close_stag = close_tag;
-  }
+    pp_set_tags fmt true;
+    let sf = pp_get_formatter_stag_functions fmt () in
+    pp_set_formatter_stag_functions fmt
+    {
+        sf with
+        mark_open_stag  = open_tag;
+        mark_close_stag = close_tag;
+    }
 
-let setup_std () =
-  setup_fmt err_formatter;
-  setup_fmt std_formatter
+    let setup_std () =
+    setup_fmt err_formatter;
+    setup_fmt std_formatter
+
--- arrakis/lib/colorsh/colorsh.mli
+++ arrakis/lib/colorsh/colorsh.mli
@@ -1,10 +1,10 @@
-(** colorsh - *****************************************************************)
-(*  Copyright 2023-2024 Gurvan Debaussart (https://debauss.art)               *)
-(*  This file is distributed under the MIT license                            *)
+(* colorsh ********************************************************************)
+(* Copyright 2023-2024 Gurvan Debaussart (https://debauss.art)                *)
+(* Distributed under the MIT license                                          *)
 (******************************************************************************)
 
-val setup_fmt : Format.formatter -> unit
-  (** [setup_fmt fmt] Active every color tags on [fmt] **)
+val setup_fmt: Format.formatter -> unit
+    (** [setup_fmt fmt] Active every color tags on [fmt] **)
 
-val setup_std : unit -> unit
-  (** [setup_std] Activate color tag on stdout and stderr. **)
+val setup_std: unit -> unit
+    (** [setup_std] Activate color tag on stdout and stderr. **)

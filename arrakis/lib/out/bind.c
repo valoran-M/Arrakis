a6a146a89992dd1c0e06ceff02ee66a7f1abf484
valorant <mvalo@proton.me>
2025-01-20 22:46:25 +0100

    feat: add base for std out shell

---
+++ arrakis/lib/out/bind.c
@@ -0,0 +1,65 @@
+#include <caml/mlvalues.h>
+#include <caml/memory.h>
+
+#include <stdbool.h>
+#include <termios.h>
+#include <unistd.h>
+#include <errno.h>
+
+/*
+ * Inspired by Down :
+ * https://github.com/dbuenzli/down/blob/master/src/down_stubs.c
+ */
+
+static struct termios restore = {0};
+static bool activate = false;
+
+value caml_init_shell(value unit)
+{
+    CAMLparam1 (unit);
+    struct termios set;
+
+    if (!activate) {
+        if (!isatty(0))
+            CAMLreturn(Val_false);
+        if (tcgetattr(0, &restore) < 0)
+            CAMLreturn(Val_false);
+
+        set = restore;
+        set.c_iflag &= ~(BRKINT | ICRNL | INPCK | ISTRIP | IXON);
+        set.c_oflag &= ~(OPOST);
+        set.c_cflag |= (CS8);
+        set.c_lflag &= ~(ECHO | ICANON | IEXTEN | ISIG);
+        set.c_cc[VMIN] = 1; set.c_cc[VTIME] = 0;
+
+        if (tcsetattr(0, TCSAFLUSH, &set) < 0)
+            CAMLreturn(Val_false);
+        activate = true; ;
+    }
+    CAMLreturn(Val_true);
+}
+
+value caml_exit_shell(value unit)
+{
+    CAMLparam1(unit);
+    if (activate) {
+        if (tcsetattr(0, TCSAFLUSH, &restore) < 0)
+            CAMLreturn(Val_false);
+        activate = false;
+    }
+    CAMLreturn(Val_true);
+}
+
+value caml_readc(value unit)
+{
+    CAMLparam1 (unit);
+    unsigned char buf;
+    int ret;
+    ret = read(0, &buf, 1);
+    if (ret == 1) CAMLreturn(Val_int(buf));
+    if (ret == 0) CAMLreturn(Val_int(-1));
+    if (ret == -1 && errno == EINTR)
+        CAMLreturn(Val_int(-2));
+    CAMLreturn(Val_int(-3));
+}
+
---
+++ arrakis/lib/out/tty.ml
@@ -0,0 +1,70 @@
+module Tty = struct
+    external init  : unit -> bool = "caml_init_shell"
+    external exit  : unit -> bool = "caml_exit_shell"
+    external readc : unit -> int  = "caml_readc"
+
+    let readc () : int option =
+        match readc () with
+        | -1 | -2 -> None
+        | -3      -> assert false
+        | n       -> Some n
+end
+
+module Ansi = struct
+    type d = Left | Up | Right | Down
+
+    type a =
+        | Unknown of string
+        | Arrow   of d
+        | Char    of char
+
+    type e = Node of (int * e) list | Leaf of a
+
+    let escape =
+        Node [
+            0x5B, (* [ *)
+            Node [
+                0x41, Leaf (Arrow Up);          (* [A *)
+                0x42, Leaf (Arrow Down);        (* [B *)
+                0x43, Leaf (Arrow Right);       (* [C *)
+                0x44, Leaf (Arrow Left);        (* [D *)
+            ];
+        ]
+
+    let pp_ansi ppf (a : a) =
+        let pp = Format.fprintf in
+        match a with
+        | Unknown s -> pp ppf "unknown(%s)" s
+        | Arrow _   -> pp ppf "a"
+        | Char c    -> pp ppf "%c" c
+
+
+    let read_escape () : a =
+        let b = Buffer.create 2 in
+        let (let*) o f =
+            match o with
+            | None   -> Unknown (Buffer.contents b)
+            | Some i -> f i
+        in
+        let rec aux (e : e) (b : Buffer.t) =
+            match e with
+            | Leaf i -> i
+            | Node l ->
+                let* i = Tty.readc () in
+                let* (_, e) = List.find_opt (fun (j, _) -> i = j) l in
+                aux e b
+        in
+        aux escape b
+
+    let input () : a option =
+        match Tty.readc () with
+        | None      -> None
+        | Some 0x1B -> Some (read_escape ())
+        | Some c    -> Some (Char (Char.chr c))
+end
+
+
+let init  = Tty.init
+let exit  = Tty.exit
+let input = Ansi.input
+

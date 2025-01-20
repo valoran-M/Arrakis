06369159950343b26cb0ecf60c9cd61d31428099
Gurvan Debaussart <gurvan@debauss.art>
2025-01-20 22:01:38 +0100

    fix: update file header

--- lib/syscall/types.ml
+++ lib/syscall/types.ml
@@ -1,8 +1,6 @@
-(******************************************************************************)
-(* Copyright 2023-2024 - Arrakis contributors                                 *)
-(*                                                                            *)
-(* This file is part of Arrakis, a RISC-V simulator.                          *)
-(* It is distributed under the CeCILL 2.1 LICENSE <http://www.cecill.info>    *)
+(* Arrakis ********************************************************************)
+(* Copyright 2023-2025 Arrakis contributors                                   *)
+(* Distributed under the CeCILL 2.1 LICENSE <http://www.cecill.info>          *)
 (******************************************************************************)
 
 type syscall_ret = Exit of int | Continue
